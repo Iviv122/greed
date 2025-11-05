e7e4c0737510bc25ee2af3e6f7325fbde5314b77
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2025-11-05 11:05:08 +0100

    auto cursor resize

--- objects/player/cursor.gd
+++ objects/player/cursor.gd
@@ -1,11 +1,17 @@
-extends Node2D
+extends Sprite2D 
 class_name Cursor
 
 @export var grid: Grid
 
+var pos : Vector2
+
+func _ready():
+	scale = Vector2(grid.size,grid.size) / texture.get_size()
+
 func _input(event):
 	if event is InputEventMouseMotion:
 		var pos = get_global_mouse_position()
-		var x = ceil((pos.x - grid.size / 2) / grid.size)
-		var y = ceil((pos.y - grid.size / 2) / grid.size)
-		global_position = Vector2(x * grid.size, y * grid.size)
+		pos.x = ceil((pos.x - grid.size / 2.) / grid.size)
+		pos.y = ceil((pos.y - grid.size / 2.) / grid.size)
+		global_position = Vector2(pos.x * grid.size, pos.y * grid.size)
+		print(pos)
